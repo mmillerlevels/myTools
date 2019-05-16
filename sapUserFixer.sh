c17bcc45cdc9541242bb7465aef05b3266ec10fe
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-05-16 14:44:26 -0600

    Create sapUserFixer.sh

---
+++ sapUserFixer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INPUT="$1"
+OLDIFS=$IFS
+IFS=,
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read ad_user email portal_id sap_email
+do
+	echo "######################"
+	echo "ad_user : $ad_user"
+	USERID=$(psql -U reachengine -d studio -c "SELECT id FROM security_user WHERE user_name = '$ad_user'" -tA)
+	echo "$USERID"
+	echo "Making Updates ~ "
+	psql -U reachengine -d studio -c "UPDATE security_user SET user_name = '$portal_id' WHERE id = $USERID" -tA
+	psql -U reachengine -d studio -c "UPDATE security_user SET email_address = '$sap_email' WHERE id = $USERID" -tA
+	echo "$ad_user, $email, $portal_id $sap_email"
+	psql -U reachengine -d studio -c "SELECT id, user_name, email_address FROM security_user WHERE id = $USERID" -tA
+	echo "######################"
+done < $INPUT
+IFS=$OLDIFS
