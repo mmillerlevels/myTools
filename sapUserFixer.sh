0a0e702be15d3df8b12f36ab4cb8e983a7346082
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-05-16 15:27:56 -0600

    Update sapUserFixer.sh

--- sapUserFixer.sh
+++ sapUserFixer.sh
@@ -10,9 +10,10 @@ do
 	echo "ad_user : $ad_user"
 	USERID=$(psql -U reachengine -d studio -c "SELECT id FROM security_user WHERE user_name = '$ad_user'" -tA)
 	echo "$USERID"
+	psql -U reachengine -d studio -c "SELECT id, user_name, email_address FROM security_user WHERE id = $USERID" -tA
 	echo "Making Updates ~ "
-	psql -U reachengine -d studio -c "UPDATE security_user SET user_name = '$portal_id' WHERE id = $USERID" -tA
-	psql -U reachengine -d studio -c "UPDATE security_user SET email_address = '$sap_email' WHERE id = $USERID" -tA
+	psql -U reachengine -d studio -c "UPDATE security_user SET user_name = '$portal_id' WHERE id = $USERID" -tAq
+	psql -U reachengine -d studio -c "UPDATE security_user SET email_address = '$sap_email' WHERE id = $USERID" -tAq
 	echo "$ad_user, $email, $portal_id $sap_email"
 	psql -U reachengine -d studio -c "SELECT id, user_name, email_address FROM security_user WHERE id = $USERID" -tA
 	echo "######################"
