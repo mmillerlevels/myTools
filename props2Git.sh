7845f9bdd558f084807c3b19ec3c115e1a14fdf7
Mike Miller <mmiller@levelsbeyond.com>
2019-07-01 17:48:01 -0600

    Do stuff

--- props2Git.sh
+++ props2Git.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+#Things
 GIT=`which git`
 REPO_DIR=/reachengine/propBkps/
+
+#Grab the latest files we want
+/bin/cat /reachengine/tomcat/conf/context.xml > ${REPO_DIR}context.xml
+/bin/cat /reachengine/tomcat/lib/dynamic.reach-engine.properties > ${REPO_DIR}dynamic.reach-engine.properties
+/bin/cat /reachengine/tomcat/lib/localContext.xml > ${REPO_DIR}localContext.xml
+/bin/cat /reachengine/tomcat/lib/log4j.properties > ${REPO_DIR}log4j.properties
+/bin/cat /reachengine/tomcat/lib/local.reach-engine.properties > ${REPO_DIR}local.reach-engine.properties
+/bin/cat /reachengine/tomcat/conf/server.xml > ${REPO_DIR}server.xml
+/bin/cat /reachengine/wfruntime/conf/local-applicationContext.xml > ${REPO_DIR}local-applicationContext.xml
+/bin/cat /reachengine/wfruntime/conf/dynamic.wferuntime.properties > ${REPO_DIR}dynamic.wferuntime.properties
+/bin/cat /reachengine/wfruntime/conf/log4j.properties > ${REPO_DIR}log4j.properties
+/bin/cat /reachengine/wfruntime/conf/local.wferuntime.properties > ${REPO_DIR}local.wferuntime.properties
+
+echo "Update Done!  `date`" >> ${REPO_DIR}commitLog.log
+
+#Do the git stuff
 cd ${REPO_DIR}
 ${GIT} add --all .
 ${GIT} commit -m "Daily Update"
-${GIT} push git@github:username/repo.git master
+${GIT} push git@github.com:username/repo.git master
