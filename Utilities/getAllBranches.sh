8eacacad8686f79a7ad8be52f8884c3c0ea8f06e
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-12-19 00:04:18 -0700

    Update getAllBranches.sh

--- Utilities/getAllBranches.sh
+++ Utilities/getAllBranches.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+## Run after your typical git clone (Within the repo dir)
+
 for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
    git branch --track ${branch#remotes/origin/} $branch
 done
